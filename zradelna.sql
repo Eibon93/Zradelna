3803337186e08aec286766f5ebe48d8869ee93e5
Eibon93 <Eibon93@users.noreply.github.com>
2025-01-16 10:24:04 +0100

    update 16/01/2025

--- zradelna.sql
+++ zradelna.sql
@@ -1,7 +1,15 @@
+<br />
+<b>Warning</b>:  "continue" targeting switch is equivalent to "break". Did you mean to use "continue 2"? in <b>C:\xamppOld\htdocs\adminer\index.php</b> on line <b>1163</b><br />
+-- Adminer 4.6.0 MySQL dump
+
 SET NAMES utf8;
 SET time_zone = '+00:00';
 SET foreign_key_checks = 0;
 SET sql_mode = 'NO_AUTO_VALUE_ON_ZERO';
+
+DROP DATABASE IF EXISTS `zradelna`;
+CREATE DATABASE `zradelna` /*!40100 DEFAULT CHARACTER SET utf8 COLLATE utf8_czech_ci */;
+USE `zradelna`;
 
 DROP TABLE IF EXISTS `menu`;
 CREATE TABLE `menu` (
@@ -13,6 +21,9 @@ CREATE TABLE `menu` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_czech_ci;
 
+INSERT INTO `menu` (`id`, `name`, `description`, `image_url`, `date`) VALUES
+(1,	'Smažený sýr',	'neco',	'',	'2025-01-15 13:00:05'),
+(2,	'Španělský ptáček',	'neco',	'',	'2025-01-15 13:00:33');
 
 DROP TABLE IF EXISTS `type_user`;
 CREATE TABLE `type_user` (
@@ -21,6 +32,9 @@ CREATE TABLE `type_user` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_czech_ci;
 
+INSERT INTO `type_user` (`id`, `name`) VALUES
+(1,	'Admin'),
+(2,	'User');
 
 DROP TABLE IF EXISTS `users`;
 CREATE TABLE `users` (
@@ -30,6 +44,9 @@ CREATE TABLE `users` (
   `type_user_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_czech_ci;
 
+INSERT INTO `users` (`id`, `email`, `pass`, `type_user_id`) VALUES
+(0,	'neco@neco',	'',	0),
+(0,	'neco2@neco',	'',	1);
 
 DROP TABLE IF EXISTS `user_choice`;
 CREATE TABLE `user_choice` (
@@ -41,3 +58,5 @@ CREATE TABLE `user_choice` (
   `date` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_czech_ci;
 
+
+-- 2025-01-16 09:22:09
