288ac4f5b3a20be2f8efe1c3e30a0a3d9dc389c7
HSko <103035284+HSko27@users.noreply.github.com>
2025-01-17 09:43:36 +0100

    Update AdminPage.tsx
    Admin 1. verze

--- client/src/pages/AdminPage.tsx
+++ client/src/pages/AdminPage.tsx
@@ -1,15 +1,37 @@
+const App = () => {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+      <form className="bg-white p-6 rounded-lg shadow-md w-96">
+        <div className="mx-auto flex">
+        <h1 className="text-2xl mx-auto font-bold mb-4 text-gray-700">Formulář</h1>
+        </div>
+        <div className="mb-4">
+          <label htmlFor="food1" className="block text-sm font-medium text-gray-600 mb-2">Jídlo 1</label>
+          <input
+            type="text"
+            id="food1"
+            className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-orange-500"
+            placeholder="Zadejte text"
+          />
+        </div>
+        <div className="mb-4">
+          <label htmlFor="food2" className="block text-sm font-medium text-gray-600 mb-2">Jídlo 2</label>
+          <input
+            type="text"
+            id="food2"
+            className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-orange-500"
+            placeholder="Zadejte text"
+          />
+        </div>
+        <button
+          type="submit"
+          className="w-full bg-[#F5A618] text-white py-2 px-4 rounded-lg hover:bg-orange-600 transition duration-200"
+        >
+          Odeslat
+        </button>
+      </form>
+    </div>
+  );
+};
 
-const AdminPage = () => {
-
-
-
-    return (
-        <>
-            <div>
-                Admin page
-                Text1
-            </div>
-        </>
-    )
-}
-export default AdminPage;
+export default App;
