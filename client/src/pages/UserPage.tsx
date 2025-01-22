aba550a444f09248b45fe9efa8a5801c3a4366f4
JustKordy <honajsek@seznam.cz>
2025-01-22 12:39:11 +0100

    poznamky pro userpage

--- client/src/pages/UserPage.tsx
+++ client/src/pages/UserPage.tsx
@@ -2,13 +2,15 @@
 
 
 const UserPage = () => {
-
+    // pouziva se tailwind pro styly
+    // bude se tu zobrazovat aktulni data o prihlasenem uzivateli, a bude tu tlacitko na zmenu hesla, emailu, atd.. proste user management
+    // bude tu kolik objednave uz uzivatel udelal, kolik ho to stalo, atd.. atd.. 
 
 
     return (
         <>
-            <div>
-                User page
+            <div className="text-red-500">
+                User pageadads
             </div>
         </>
     )
